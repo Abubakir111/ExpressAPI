d111bc5ba95efde18450a22afcaddc610fc5b95f
Бакир Базаркул <bakirbazarkul16@gmail.com>
2024-10-08 03:01:11 +0300

    Initial commit2

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Простой маршрут для проверки
+// Middleware для парсинга JSON
+app.use(express.json());
+
+// Обработчик для GET-запроса на корневой маршрут
 app.get('/', (req, res) => {
-  res.send('Hello, world!'); // Или верните HTML-контент
+  res.send('Hello, world!');
+});
+
+// Обработчик для GET-запроса на /data
+app.get('/data', (req, res) => {
+  const dataArray = [
+    { id: 1, name: 'Тестовый запрос на удалённом сервере API' },
+    { id: 2, name: 'Еще один тестовый запрос' }
+    // Добавьте другие элементы, если нужно
+  ];
+  res.json(dataArray); // Отправляем массив данных в формате JSON
 });
 
 // Запускаем сервер
